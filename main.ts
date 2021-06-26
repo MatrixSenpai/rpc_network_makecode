f4a6a76ef8c43e4e18f3199e47ebdbaf52e3bf22
Mason Phillips <math.matrix@icloud.com>
2021-06-26 14:40:48 -0500

    updating to allow users to choose their own choice

--- main.ts
+++ main.ts
@@ -4,45 +4,47 @@
  * Rock = 0, Paper = 1, Scissors = 2
  */
 
-let didThrow = false
-let hand = 0
-
+// Initialization
+let didThrow = false;
+let isLockedIn = false;
+let isOpponentLockedIn = false;
+let hand = -1;
+let opponentHand = -1;
 radio.setGroup(1)
 
-radio.onReceivedNumber(function (num) {
-    if(!didThrow) throwHand();
+/**
+ * Helper Functions
+ * 
+ * Handles display, winners, comms
+ */
 
-    showThrow(hand);
-    basic.pause(1000)
+const handleThrow = () => {
+    basic.showString("RPS!")
+    basic.pause(500);
 
-    if (hand == 0 && num == 1)
+    console.log(hand);
+    console.log(opponentHand);
+
+    if (hand == 0 && opponentHand == 1)
         showWon(false);
-    if (hand == 0 && num == 2)
+    if (hand == 0 && opponentHand == 2)
         showWon(true);
-    if (hand == 1 && num == 0)
+    if (hand == 1 && opponentHand == 0)
         showWon(true);
-    if (hand == 1 && num == 2)
+    if (hand == 1 && opponentHand == 2)
         showWon(false);
-    if (hand == 2 && num == 0)
+    if (hand == 2 && opponentHand == 0)
         showWon(false);
-    if (hand == 2 && num == 1)
+    if (hand == 2 && opponentHand == 1)
         showWon(true);
 
-    if (hand == num)
+    if (hand == opponentHand)
         basic.showString("Draw!")
 
-    didThrow = false
-});
-
-input.onButtonPressed(Button.A, function () {
-    throwHand();
-})
-
-const throwHand = () => {
-    basic.clearScreen();
-    hand = randint(0, 2);
-    radio.sendNumber(hand);
-    didThrow = true;
+    isLockedIn = false;
+    isOpponentLockedIn = false;
+    hand = -1;
+    opponentHand = -1;   
 }
 
 const showThrow = (num: number) => {
@@ -83,3 +85,48 @@ const showWon = (didWin: boolean) => {
     else
         basic.showString("LOSER!");
 }
+
+
+/**
+ * Radio Handlers
+ * 
+ * Handles getting and sending number, getting and sending state
+ */
+
+/// Receive opponent ready state
+radio.onReceivedMessage(1, () => {
+    isOpponentLockedIn = true;
+    radio.sendNumber(hand);
+});
+
+/// Receive opponent choice
+radio.onReceivedNumber((num: number) => {
+    opponentHand = num;
+    console.log(`${control.deviceName()}: ${num}`);
+});
+
+/**
+ * Input Handlers
+ * 
+ * Choose your RPS and lock it in
+ */
+
+/// Swaps between rock, paper, and scissors
+input.onButtonPressed(Button.A, function () {
+    if(isLockedIn) return;
+    hand += 1;
+    if(hand >= 3) hand = 0;
+
+    showThrow(hand);
+});
+
+/// Sends a signal to the other player that you are ready
+input.onButtonPressed(Button.B, function () {
+    isLockedIn = true;
+    radio.sendMessage(1)
+});
+
+basic.forever(function () {
+    if(isLockedIn && isOpponentLockedIn)
+        handleThrow();
+})
